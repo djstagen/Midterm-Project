d5d536aab9317469b68721dfe31fb2646f060c12
djstagen <58088743+djstagen@users.noreply.github.com>
2020-03-01 23:49:06 -0600

    Attempted making road repeat, got an error

---
+++ Assets/Scripts/RepeateBackground.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RepeateBackground : MonoBehaviour
+{
+    private Vector3 startPos;
+    private float repeatLength;
+    // Start is called before the first frame update
+    void Start()
+    {
+        startPos = transform.position;
+        repeatLength = GetComponent<BoxCollider>().size.x / 2;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.z < startPos.z - repeatLength)
+        {
+            transform.position = startPos;
+        }
+    }
+}
