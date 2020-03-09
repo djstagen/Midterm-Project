d37116707b7b77f97627f66de9ce926e27fa91d2
djstagen <58088743+djstagen@users.noreply.github.com>
2020-03-08 22:50:01 -0500

    Milestone2

--- Assets/Scripts/DetectCollision.cs
+++ Assets/Scripts/DetectCollision.cs
@@ -1,22 +1,37 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class DetectCollision : MonoBehaviour
 {
+    ScoreManager scoreManager; 
     // Start is called before the first frame update
     void Start()
     {
-        
+        GameObject Object = GameObject.FindGameObjectWithTag("Player");
+
+        scoreManager = Object.GetComponent<ScoreManager>();
     }
 
-    private void OnTriggerEnter(Collider other)
+    private void OnTriggerEnter(Collider collision)
     {
+        GameObject Object = collision.gameObject;
+        Debug.Log($"{gameObject.name} just hit {Object.name}.");
         //It will work with rigidBody
-
         Destroy(gameObject);
-        //Destroy(other.gameObject);
+        scoreManager.IncrementScore();
+
+        //This will not work with any tag.
+       /* if (Object.CompareTag("Object"))
+        {
+            Destroy(gameObject);
+            scoreManager.IncrementScore();
+            //Destroy(other.gameObject);
+        }*/
+
     }
+
 
     // Update is called once per frame
     void Update()
---
+++ Assets/Scripts/ScoreManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreManager : MonoBehaviour
+{
+    [SerializeField] Text scoreText;
+    [SerializeField] private int score;
+    // Start is called before the first frame update
+    void Start()
+    {
+        score = 0;
+        UpdateText();
+    }
+
+    public void IncrementScore()
+    {
+        score++;
+        //score +=1;
+        //score = score +1;
+        UpdateText();
+    }
+
+    public void DecrementScore()
+    {
+        score--;
+        //score -= 1;
+        //score = score -1;
+        UpdateText();
+    }
+
+    // Update is called once per frame
+    private void UpdateText()
+    {
+        scoreText.text = $"Sanity/Comfort: {score}";
+    }
+}
